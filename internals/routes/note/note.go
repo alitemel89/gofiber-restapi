6298c82b886db9c87da91c1c1ad4b5859fa1feb8
alitemel89 <alitemel89.ce@gmail.com>
2021-12-01 00:18:25 +0300

    update setupnoteroutes function

--- internals/routes/note/note.go
+++ internals/routes/note/note.go
@@ -2,9 +2,12 @@ package noteRoutes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	noteHandler "github.com/alitemel89/gofiber-restapi/internals/handlers/note"
 )
 
 
 func SetupNoteRoutes(router fiber.Router) {
-
+	note := router.Group("/note")
+    // Create a Note
+    note.Post("/", noteHandler.CreateNotes)
 }
